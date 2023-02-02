d96577ea1146dd919ee0520b8b3a6b47a59f79cd
francescogentile25 <francescogentile250893@gmail.com>
2023-02-02 02:38:19 +0100

    STYLE harry potter added to page

--- js/ticket.js
+++ js/ticket.js
@@ -1,36 +1,48 @@
 const formBtnElement = document.querySelector('input[type="button"]')
+const price = document.getElementById('price')
+// console.log(sale, price)
+const priceSingleKm=0.21
 
-// const boxElement = document.querySelector('#example-2 .box')
-const sale = document.querySelector('#sale')
-const price = document.querySelector('#price')
-console.log(sale, price)
-// const inputWidthElement = document.querySelector(
-// 	'#example-2 input[name="distance"]'
-// )
 const inputKmElement = document.querySelector(
 	'#example-2 input[name="distance"]'
 )
+const inputName = document.querySelector(
+	'#example-2 input[name="passenger"]'
+)
+console.log(inputName)
 const selectAgeElement = document.querySelector(
 	'#example-2 select[name="age"]'
 )
+const name = document.getElementById('name')
+// console.log(formBtnElement, inputKmElement, selectAgeElement)
 
-console.log(formBtnElement, inputKmElement, selectAgeElement)
 
 formBtnElement.addEventListener('click', function () {
-	console.log('Il bottone funziona')
-	console.log(inputKmElement.value, selectAgeElement.value)
-    
+	// console.log('Il bottone funziona')
+	console.log(inputKmElement.value, selectAgeElement.value,inputName.value )
+	name.innerHTML=( 'Benvenuto a bordo: ' +inputName.value  )
     let distance = inputKmElement.value
-	let price = selectAgeElement.value
+	const lenght =document.getElementById('tripLenght')
+	lenght.innerHTML= (distance  + ' Km')
+    //console.log(distance)
+	let discountSelection = selectAgeElement.value
+	const planeSelected = document.getElementById("sale")
+	const selectedOption = document.querySelector("option:checked");
+	planeSelected.innerText = 'Hai scelto la tariffa: ' + selectedOption.text;
+	
 
-	switch (price) {
+	switch (discountSelection) {
 		case 'youngh':
-			
+            const youngPrice=priceSingleKm* distance * 0.8
+            //console.log(youngPrice)
+            price.innerHTML= (youngPrice.toFixed(2) + '&euro;')
 			break
 		case 'senior':
-			
+			const senioPrice=priceSingleKm*distance*0.6
+			price.innerHTML=(senioPrice.toFixed(2)  + '&euro;')
 			break
 		default:
-			
+			const normalPrice= priceSingleKm*distance
+			price.innerHTML=(normalPrice.toFixed(2)  + '&euro;')
 	}
 })
